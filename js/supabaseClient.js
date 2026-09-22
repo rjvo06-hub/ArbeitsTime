4cc99bdc00cd318b19c2bfc50521dc71eb6607ee
rjvo06-hub <rjvo06@gmail.com>
2026-09-23 01:34:32 +0200

    Add files via upload

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Reemplaza con tus credenciales de Supabase
-const SUPABASE_URL = 'https://tu-proyecto.supabase.co';
-const SUPABASE_ANON_KEY = 'tu-clave-anonima-publica';
+const SUPABASE_URL = 'https://ymegydnuzjcdugniluwj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_yAbvsk8wmlMmi3gGXxOPEw_5yyVG5vF';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
